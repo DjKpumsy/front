92f04dde2273eac1f649de7fb441e62ebde1a1f7
DjKpumsy <101316855+DjKpumsy@users.noreply.github.com>
2024-06-14 17:26:24 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -68,7 +68,7 @@ function App() {
                         return prevProgress;
                     }
                 });
-            }, 5000); // Adjust the refill speed as needed
+            }, 3000); // Adjust the refill speed as needed
             setRefillInterval(intervalId);
         }
     };
