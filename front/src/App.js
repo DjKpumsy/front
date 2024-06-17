bd96da869d4a8df36e24b82569b7c438f2f5a920
DjKpumsy <101316855+DjKpumsy@users.noreply.github.com>
2024-06-17 23:26:16 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -119,7 +119,7 @@ function App() {
                 <img 
                     src={buttonImage} 
                     alt="Earn Points" 
-                    style={{ cursor: 'pointer', width: '40px', height: '40px', marginBottom: '-2px', marginRight: '-30px' }} 
+                    style={{ cursor: 'pointer', width: '40px', height: '40px', marginBottom: '-2px', marginRight: '-40px' }} 
                 /> 
                 {points} 
             </h1>
