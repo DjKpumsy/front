75d33ba4c462133a06b8367f22a2d228ffc74f6d
DjKpumsy <101316855+DjKpumsy@users.noreply.github.com>
2024-06-17 23:45:59 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -115,7 +115,7 @@ function App() {
 
     return (
         <div className="App">
-            <h1>  
+            <h1 style={{top: '300px'}}>  
                 <img 
                     src={buttonImage} 
                     alt="Earn Points" 
